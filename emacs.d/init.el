14e4072c95e4950323fab557d72074ef2d2b88e8
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2012-03-02 12:37:47 +0900

    Add emacs initial file.

---
+++ emacs.d/init.el
@@ -0,0 +1,56 @@
+;;; This is Emacs Setting file.
+
+;;; Local variable
+(setq conf-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+;;; Set load-path
+(add-to-list 'load-path conf-dir)
+
+;;; Define keymap
+(define-key global-map "\C-h" 'delete-backward-char)
+(define-key global-map "\C-x\C-h" 'help-command)
+(define-key global-map "\M-/" 'dabbrev-expand)
+
+;;; User Interface Setting
+(tool-bar-mode nil)
+;(menu-bar-mode nil)
+
+;;; Color Setting
+(when (fboundp 'global-font-lock-mode)
+  (global-font-lock-mode t))
+
+;;; Communication with external program
+(setq diff-switches "-u")
+
+;;; Buffer behavior
+(when (require 'uniquify)
+  (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
+
+;;; Code ornament
+(global-font-lock-mode t)
+(show-paren-mode t)
+(setq show-paren-style 'mixed)
+(transient-mark-mode t)
+(column-number-mode t)
+;; Display line number
+;(when (require 'linum) (global-linum-mode t))
+
+;;; Code behavior
+(delete-selection-mode t)
+(setq require-final-newline t)
+
+;;; Completion
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+;; ex. p-b -> print-buffer
+(partial-completion-mode t)
+;; display ordinary
+(icomplete-mode t)
+
+;;; History
+(setq history-length 10000)
+(savehist-mode t)
+(setq recentf-max-saved-items 10000)
+
+;;; Compression
+(auto-compression-mode t)

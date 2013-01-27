af346a15c5ce50fcbf86201dc377f45f6bfff09d
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2013-01-27 17:01:37 +0900

    Changed many settingfiles mainly emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,33 +1,38 @@
 ;;; This is Emacs Setting file.
 
 ;;; Local variable
-(setq conf-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(when (< emacs-major-version 23)
+  (defvar user-emacs-directory (file-name-directory (or buffer-file-name load-file-name))))
 
 ;;; Set load-path
-(add-to-list 'load-path conf-dir)
-(add-to-list 'load-path (concat conf-dir "elisps/"))
-(add-to-list 'load-path (concat conf-dir "packages/"))
-(let ((default-directory (concat conf-dir "packages/")))
-  (normal-top-level-add-subdirs-to-load-path))
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path (concat user-emacs-directory "elisps/"))
+(when (< emacs-major-version 24)	; for no 'package env.
+ (add-to-list 'load-path (concat user-emacs-directory "packages/"))
+ (let ((default-directory (concat user-emacs-directory "packages/")))
+   (normal-top-level-add-subdirs-to-load-path)))
 
-;;; Define keymap
-(define-key global-map "\C-h" 'delete-backward-char)
-(define-key global-map "\C-x\C-h" 'help-command)
-(define-key global-map "\M-/" 'dabbrev-expand)
+;;; Package Managing
+(when (require 'package nil t)
+  (add-to-list 'package-archives '("melpa". "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade". "http://marmalade-repo.org/packages/") t)
+  (setq package-user-dir(concat user-emacs-directory "packages/"))
+  (package-initialize))
 
 ;;; Font Setting
 (when (equal window-system 'x)
-  (load-file (concat conf-dir "fonts.el"))
+  (load-file (concat user-emacs-directory "fonts.el"))
   (set-frame-font "Inconsolata"))
 
 ;;; User Interface Setting
-(tool-bar-mode nil)
-;(menu-bar-mode nil)
+(if (< emacs-major-version 24) (tool-bar-mode nil) (tool-bar-mode 0)
+(menu-bar-mode nil))
 
 ;;; Color Setting
 (when (fboundp 'global-font-lock-mode)
   (global-font-lock-mode t))
-(when (require 'color-theme)
+;; Extension: color-theme
+(when (require 'color-theme nil t)	; Extension: color-theme
   (color-theme-initialize)
   (color-theme-deep-blue))
 
@@ -38,7 +43,7 @@
 (when (require 'uniquify)
   (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
 
-;;; Code ornament
+;;; Code ornament / Visual
 (global-font-lock-mode t)
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
@@ -46,16 +51,22 @@
 (column-number-mode t)
 ;; Display line number
 ;(when (require 'linum) (global-linum-mode t))
+(setq scroll-conservatively 1)
 
 ;;; Code behavior
 (delete-selection-mode t)
 (setq require-final-newline t)
 
 ;;; Completion
+(when (require 'auto-complete nil t)		; Extension: auto-complete
+  (add-to-list 'ac-dictionary-directories (concat user-emacs-directory "acdict/"))
+  (require 'auto-complete-config)
+  (ac-config-default)
+  (define-key ac-mode-map "\M-/" 'auto-complete))
 (setq completion-ignore-case t)
 (setq read-file-name-completion-ignore-case t)
 ;; ex. p-b -> print-buffer
-(partial-completion-mode t)
+;(partial-completion-mode t)
 ;; display ordinary
 (icomplete-mode t)
 
@@ -66,3 +77,8 @@
 
 ;;; Compression
 (auto-compression-mode t)
+
+;;; Define global keymap
+(define-key global-map "\C-h" 'delete-backward-char)
+(define-key global-map "\C-x\C-h" 'help-command)
+;(define-key global-map "\M-/" 'dabbrev-expand)

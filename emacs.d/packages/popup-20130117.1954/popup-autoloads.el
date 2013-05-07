21134ac7e57c022f22cf34dcc06d978540063c25
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2013-05-08 02:09:01 +0900

    remove package from git

--- emacs.d/packages/popup-20130117.1954/popup-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/packages/popup-20130117.1954/popup-pkg.el"
-;;;;;;  "../../../../.emacs.d/packages/popup-20130117.1954/popup.el")
-;;;;;;  (20781 22647 12104 577000))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here

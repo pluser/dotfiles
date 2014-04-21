bb09ba063f355ca8c1dc09c3691f587480660b0f
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2014-04-22 03:17:04 +0900

    [emacs] add terminal emulation setting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -108,6 +108,10 @@
 ;; Display line number
 ;(when (require 'linum) (global-linum-mode t))
 
+;;; Terminal Emulator
+;(when (require 'term+ nil t)
+;  (require 'xterm-256color))
+
 ;;; Behavior
 ;(delete-selection-mode nil)
 (setq require-final-newline nil)
@@ -146,3 +150,26 @@
 
 ;;; Dired Settings
 (setq dired-listing-switches "-alh")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+ '(explicit-shell-file-name "/bin/zsh"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compilation-error ((t (:foreground "yellow"))) t)
+ '(error ((t (:foreground "yellow" :weight bold))))
+ '(term-color-black ((t (:background "gray5" :foreground "gray5"))))
+ '(term-color-blue ((t (:background "slate blue" :foreground "slate blue"))))
+ '(term-color-cyan ((t (:background "deep sky blue" :foreground "deep sky blue"))))
+ '(term-color-green ((t (:background "SpringGreen3" :foreground "SpringGreen3"))))
+ '(term-color-magenta ((t (:background "maroon1" :foreground "maroon1"))))
+ '(term-color-red ((t (:background "#ee5533" :foreground "#ee5533"))))
+ '(term-color-white ((t (:background "gainsboro" :foreground "gainsboro"))))
+ '(term-color-yellow ((t (:background "yellow2" :foreground "yellow2")))))

1168ed047db8164a602c93524650a890d4b0c7c5
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2014-05-09 03:11:20 +0900

    [emacs] refactoring config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
-;;; -*- mode: Emacs-Lisp; coding: -*- ;;;
+;;; -*- mode: Emacs-Lisp; coding: utf-8 -*- ;;;
 ;;; init.el --- A emacs setting file
 
 ;;; Local variable
@@ -23,6 +23,10 @@
   (add-to-list 'package-archives '("marmalade". "http://marmalade-repo.org/packages/") t)
   (setq package-user-dir (concat user-emacs-directory "packages/"))
   (package-initialize))
+(defmacro package-config (package &rest body)
+  "Load and set package settings."
+  (declare (indent defun))
+  `(eval-after-load ,package (lambda () ,@body)))
 
 ;;; Font Setting
 (when (and (display-graphic-p) (file-readable-p (concat user-emacs-directory "fonts.el")))
@@ -43,50 +47,13 @@
 ;;; Language Setting
 (set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
-(when (require 'skk-autoloads nil t)
-  (setq skk-user-directory (concat user-emacs-directory "ddskk/"))
-  (setq default-input-method "japanese-skk")
-  (define-key global-map "\C-x\C-j" 'skk-mode)
-  (setq skk-henkan-show-candidates-keys (list ?a ?o ?e ?u ?h ?t ?n ?s))
-  (setq skk-indicator-use-cursor-color nil)
-  (setq skk-kakutei-key (kbd "C-t"))
-  (setq skk-background-mode 'dark) ; For DDSKK 15.1 Hot-Fix
-  (require 'skk-hint nil t)
-  (when (require 'skk-search-web nil t)
-;    (add-to-list 'skk-search-prog-list '(skk-search-web 'skk-google-cgi-api-for-japanese-input) t)
-    (add-to-list 'skk-search-prog-list '(skk-search-web 'skk-google-suggest) t)))
 
 ;;; User Interface Setting
+(setq transient-mark-mode nil)
 ;(setq visible-bell t)
 (setq ring-bell-function 'ignore)
-(when (require 'helm nil t)		; Extension: helm
-  (require 'helm-config))
 (if (< emacs-major-version 24) (tool-bar-mode nil) (tool-bar-mode 0))
 (if (< emacs-major-version 24) (menu-bar-mode nil) (menu-bar-mode 0))
-(when (require 'evil nil t)		; Extension: evil
-  (defun evil-execute-in-normal-state-at-emacs-state ()
-    "Execute the next command in Normal state at Emacs state."
-    (interactive)
-    (evil-delay '(not (memq this-command
-			    '(evil-execute-in-normal-state-at-emacs-state
-			      evil-use-register
-			      digit-argument
-			      negative-argument
-			      universal-argument
-			      universal-argument-minus
-			      universal-argument-more
-			      universal-argument-other-key)))
-		`(progn
-		   (evil-emacs-state)
-		   (setq evil-move-cursor-back ',evil-move-cursor-back))
-		'post-command-hook)
-    (setq evil-move-cursor-back nil)
-    (evil-normal-state)
-    (evil-echo "Command?"))
-;  (global-set-key (kbd "<henkan>") 'evil-execute-in-normal-state-at-emacs-state)
-  (define-key evil-normal-state-map (kbd "<henkan>") 'evil-emacs-state)
-  (define-key evil-emacs-state-map (kbd "<henkan>") 'evil-normal-state)
-  (setq evil-default-state 'emacs))
 
 ;;; Color / Theme Setting
 (if (< emacs-major-version 24)
@@ -95,43 +62,30 @@
       (color-theme-deep-blue))
   (load-theme 'deeper-blue))
 
-;;; Communication with external program
-(setq diff-switches "-u")
+;;; File Opener
+(when (require 'filecache nil t)	; Extension: filecache
+  (file-cache-add-file (concat user-emacs-top-directory "init.el")))
 
-;;; Buffer behavior
-(when (require 'uniquify)
-  (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
+;;; Diff Setting
+(setq diff-switches "-u")
 
 ;;; Code ornament / Visual
 (global-font-lock-mode t)
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
-;(transient-mark-mode -1)
 (setq scroll-conservatively 0)
 (line-number-mode t)
 (column-number-mode t)
 ;; Display line number
-;(when (require 'linum) (global-linum-mode t))
+;(when (require 'linum nil t) (global-linum-mode t))
 
-;;; Terminal Emulator
-;(when (require 'term+ nil t)
-;  (require 'xterm-256color))
-
-;;; Behavior
-;(delete-selection-mode nil)
+;;; Code styling
 (setq require-final-newline nil)
 
 ;;; Completion
-(when (require 'auto-complete nil t)		; Extension: auto-complete
-;  (add-to-list 'ac-dictionary-directories (concat user-emacs-directory "acdict/"))
-  (require 'auto-complete-config)
-  (ac-config-default)
-  (define-key ac-mode-map "\M-/" 'auto-complete))
 (setq completion-ignore-case t)
 (setq read-file-name-completion-ignore-case t)
-;; ex. p-b -> print-buffer
 ;(partial-completion-mode t)
-;; display ordinary
 (icomplete-mode t)
 
 ;;; History
@@ -150,8 +104,54 @@
 ;;; Load Private confidential file
 (load-file (concat user-emacs-top-directory "private/email.el"))
 
-;;; A workaround for Tramp for text corruption.
-(setq tramp-remote-process-environment (quote ("HISTFILE=$HOME/.tramp_history" "HISTSIZE=1" "LC_MESSAGE=C" "TERM=dumb" "EMACS=t" "INSIDE_EMACS='24.3.1,tramp:2.2.6-24.3'" "CDPATH=" "HISTORY=" "MAIL=" "MAILCHECK=" "MAILPATH=" "PAGER=\"\"" "autocorrect=" "correct=")))
+;;; Helm Settings
+(package-config 'helm		; Extension: helm
+  (require 'helm-config))
+
+;;; Evil Settings
+(package-config 'evil		; Extension: evil
+  (define-key evil-normal-state-map (kbd "<henkan>") 'evil-emacs-state)
+  (define-key evil-emacs-state-map (kbd "<henkan>") 'evil-normal-state)
+  (setq evil-default-state 'emacs))
+
+;;; Company Settings
+(package-config 'company		; Extension: company
+  (global-company-mode))
+
+;;; Auto-Complete Settings
+(package-config 'auto-complete		; Extension: auto-complete
+;  (add-to-list 'ac-dictionary-directories (concat user-emacs-directory "acdict/"))
+  (require 'auto-complete-config)
+  (ac-config-default)
+  (setq ac-auto-show-menu nil)
+  (setq ac-use-quick-help nil)
+  (define-key ac-mode-map "\M-/" 'auto-complete))
+
+;;; SKK Settings
+(package-config 'skk-autoloads		; Extension: SKK
+  (setq skk-user-directory (concat user-emacs-directory "ddskk/"))
+  (setq default-input-method "japanese-skk")
+  (define-key global-map "\C-x\C-j" 'skk-mode)
+  (setq skk-henkan-show-candidates-keys (list ?a ?o ?e ?u ?h ?t ?n ?s))
+  (setq skk-indicator-use-cursor-color nil)
+  (setq skk-kakutei-key (kbd "C-t"))
+  (setq skk-background-mode 'dark) ; For DDSKK 15.1 Hot-Fix
+  (require 'skk-hint nil t)
+  (when (require 'skk-search-web nil t)
+;   (add-to-list 'skk-search-prog-list '(skk-search-web 'skk-google-cgi-api-for-japanese-input) t)
+    (add-to-list 'skk-search-prog-list '(skk-search-web 'skk-google-suggest) t)))
+
+;;; Tramp Settings
+;; A workaround for Tramp for text corruption.
+;(setq tramp-remote-process-environment (quote ("HISTFILE=$HOME/.tramp_history" "HISTSIZE=1" "LC_MESSAGE=C" "TERM=dumb" "EMACS=t" "INSIDE_EMACS='24.3.1,tramp:2.2.6-24.3'" "CDPATH=" "HISTORY=" "MAIL=" "MAILCHECK=" "MAILPATH=" "PAGER=\"\"" "autocorrect=" "correct=")))
+
+;;; Terminal Emulator Settings
+;(when (require 'term+ nil t)
+;  (require 'xterm-256color nil t))
+
+;;; Uniquify Settings
+(when (require 'uniquify nil t)
+  (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
 
 ;;; Dired Settings
 (setq dired-listing-switches "-alh")

a93cc7c945c3e6780431f1e4c8093336bc7c24e2
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2015-10-09 16:41:31 +0900

    [emacs] change face for flycheck

--- emacs.d/customize.el
+++ emacs.d/customize.el
@@ -10,6 +10,7 @@
  '(company-preview-common ((t (:inherit company-preview :foreground "orange"))))
  '(compilation-error ((t (:foreground "yellow"))))
  '(error ((t (:foreground "yellow" :weight bold))))
+ '(flycheck-error ((t (:underline (:color "chartreuse" :style wave)))))
  '(term-color-black ((t (:background "gray5" :foreground "gray5"))))
  '(term-color-blue ((t (:background "slate blue" :foreground "slate blue"))))
  '(term-color-cyan ((t (:background "deep sky blue" :foreground "deep sky blue"))))
@@ -17,7 +18,8 @@
  '(term-color-magenta ((t (:background "maroon1" :foreground "maroon1"))))
  '(term-color-red ((t (:background "#ee5533" :foreground "#ee5533"))))
  '(term-color-white ((t (:background "gainsboro" :foreground "gainsboro"))))
- '(term-color-yellow ((t (:background "yellow2" :foreground "yellow2")))))
+ '(term-color-yellow ((t (:background "yellow2" :foreground "yellow2"))))
+ '(whitespace-tab ((t (:foreground "darkgray")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,5 +35,5 @@
  '(make-backup-files nil)
  '(package-selected-packages
    (quote
-	(company-math helm-company projectile company-web company-jedi pyvenv jinja2-mode python-mode company helm web-mode sunrise-commander php-mode markdown-mode magit idomenu flycheck evil elpy dired+ dic-lookup-w3m ddskk company-c-headers ace-isearch)))
+	(yasnippet mozc-im narrow-reindent company-statistics flycheck-pyflakes company-math helm-company projectile company-web company-jedi pyvenv jinja2-mode python-mode company helm web-mode sunrise-commander php-mode markdown-mode magit idomenu flycheck evil elpy dired+ dic-lookup-w3m ddskk company-c-headers ace-isearch)))
  '(tab-width 4))

33b2cfcea4eed0a07fdff348f8e8d8cd48e62471
Kaoru Esashika <e_kaoru5@yahoo.co.jp>
2016-03-25 17:42:45 +0900

    [awesome] add AutoStart script

--- awesome.d/rc.lua
+++ awesome.d/rc.lua
@@ -444,3 +444,24 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- {{{ AutoStart
+function run_once(prg,arg_string,pname,screen)
+    if not prg then
+        do return nil end
+    end
+
+    if not pname then
+       pname = prg
+    end
+
+    if not arg_string then 
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+    else
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. " ".. arg_string .."' || (" .. prg .. " " .. arg_string .. ")",screen)
+    end
+end
+
+run_once("skype")
+run_once("blueman-applet")
+---]]]

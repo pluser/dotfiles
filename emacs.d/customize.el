204832924af9bf4c51e4c032091f2b2369a3f1e1
ESASHIKA Kaoru <e_kaoru5@yahoo.co.jp>
2020-04-02 17:48:06 +0900

    [emacs] update installed packages

--- emacs.d/customize.el
+++ emacs.d/customize.el
@@ -14,5 +14,5 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(auctex lsp-mode lsp-ui treemacs-magit counsel dashboard hydra ivy projectile swiper elpy lua-mode pipenv vterm doom-modeline doom-themes typescript-mode treemacs treemacs-evil treemacs-icons-dired treemacs-projectile yasnippet which-key web-mode magit helm evil-surround ddskk dart-mode company-lsp ccls avy)))
+	(ein ox-latex ox auctex lsp-mode lsp-ui treemacs-magit counsel dashboard hydra ivy projectile swiper elpy lua-mode pipenv vterm doom-modeline doom-themes typescript-mode treemacs treemacs-evil treemacs-icons-dired treemacs-projectile yasnippet which-key web-mode magit helm evil-surround ddskk dart-mode company-lsp ccls avy)))
  '(skk-jisyo-edit-user-accepts-editing t))

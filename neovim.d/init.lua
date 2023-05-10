d0dbf61c701e3e318b67286e1e324aa8664da69d
ESASHIKA Kaoru <e_kaoru5@yahoo.co.jp>
2023-05-11 03:19:54 +0900

    [nvim] update

--- neovim.d/init.lua
+++ neovim.d/init.lua
@@ -14,24 +14,27 @@ vim.opt.rtp:prepend(lazypath)
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+local function fullspec()
+	return not vim.g.vscode
+end
+
 require('lazy').setup({
 { 'EdenEast/nightfox.nvim',
 	lazy = false,
 	priority = 100,
-	config = function() require('nightfox').load() end },
+	config = function(plugin, opts) require('nightfox').load() end },
 { 'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-	event = 'VeryLazy',
-	opts = { require('lualine_evil') }},
+	event = { 'VeryLazy' },
+	config = function(plugin, opts) require('lualine_evil') end },
 { 'akinsho/bufferline.nvim',
 	event = 'VeryLazy',
-	dependencies = 'nvim-tree/nvim-web-devicons',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = true },
 { 'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
-	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-	event = { 'BufNewFile', 'BufRead' },
 	config = function(name, opts) require('nvim-treesitter').setup(opts) end,
+	event = { 'BufNewFile', 'BufRead' },
 	opts = {
 		ensure_installed = {},
 		auto_install = true,
@@ -39,6 +42,7 @@ require('lazy').setup({
 		indent = { enable = true }}},
 { 'nvim-treesitter/nvim-treesitter-textobjects',
 	lazy = true,
+	event = { 'BufNewFile', 'BufRead' },
 	dependencies = { 'nvim-treesitter/nvim-treesitter' }},
 { 'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-frecency.nvim' },
@@ -59,10 +63,15 @@ require('lazy').setup({
         dependencies = { 'kkharji/sqlite.lua' }},
 { 'folke/which-key.nvim' },
 { 'williamboman/mason.nvim',
-	build = ':MasonUpdate'},
+	event = { 'BufRead' },
+	cmd = { 'Mason', 'MasonInstall' },
+	build = ':MasonUpdate',
+	config = true },
 { 'williamboman/mason-lspconfig.nvim',
+	event = { 'LspAttach' },
 	dependencies = { 'williamboman/mason.nvim' }},
 { 'neovim/nvim-lspconfig',
+	event = { 'LspAttach' },
 	dependencies = { 'williamboman/mason-lspconfig.nvim' },
 	config = function()
 		local lsp = vim.lsp.buf
@@ -74,11 +83,37 @@ require('lazy').setup({
 { 'nvim-neo-tree/neo-tree.nvim',
 	cmd = 'Neotree',
 	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim' }},
-{ 'mfussenegger/nvim-dap' },
+{ 'mfussenegger/nvim-dap',
+	lazy = true },
 { 'rcarriga/nvim-dap-ui',
+	event = { 'LspAttach' },
 	dependencies = { 'mfussenegger/nvim-dap' }},
 { 'jay-babu/mason-nvim-dap.nvim',
-	dependencies = { 'williamboman/mason.nvim', 'mfussenegger/nvim-dap' }},
+	event = { 'LspAttach' },
+	dependencies = { 'williamboman/mason.nvim', 'mfussenegger/nvim-dap' },
+	opts = {
+		handlers = {
+			python = function(source_name)
+				local dap = require('dap')
+				dap.adapters.python = {
+					type = 'executable',
+					command = 'python3',
+					args = {
+						'-m',
+						'debugpy.adapter',
+					},
+				}
+				dap.configurations.python = {
+					{
+						type = 'python',
+						request = 'launch',
+						name = 'Launch file',
+						program = '${file}',
+					},
+				}
+			end,
+		},
+	} },
 { 'echasnovski/mini.pairs',
 	name = 'mini.pairs',
 	event = { 'InsertEnter' },
@@ -111,7 +146,6 @@ require('lazy').setup({
 { 'dcampos/nvim-snippy',
 	event = { 'InsertEnter' }},
 { 'lewis6991/gitsigns.nvim',
-	cond = function() return not vim.g.vscode end,
 	dependencies = { 'petertriho/nvim-scrollbar' },
 	event = { 'BufNewFile', 'BufRead' }},
 { 'lukas-reineke/indent-blankline.nvim',
@@ -119,7 +153,8 @@ require('lazy').setup({
 { 'TimUntersberger/neogit',
 	cmd = 'Neogit' },
 { 'SmiteshP/nvim-navic',
-        requires = 'neovim/nvim-lspconfig',
+	dependencies = { 'neovim/nvim-lspconfig' },
+	event = { 'LspAttach' },
         config = function()
                 require('mason-lspconfig').setup_handlers {
                         function(server_name) require('lspconfig')[server_name].setup {
@@ -128,13 +163,11 @@ require('lazy').setup({
                                                 require('nvim-navic').attach(client, bufnr)
                                         end end } end } end },
 { 'petertriho/nvim-scrollbar',
-        cond = function() return not vim.g.vscode end,
 	event = { 'BufWinEnter', 'CmdwinLeave', 'TabEnter', 'TermEnter', 'TextChanged', 'VimResized', 'WinEnter', 'WinScrolled' },
 	name = 'scrollbar' },
 { 'kevinhwang91/nvim-hlslens',
 	enabled = false,
-	name = 'hlslens',
-	cond = function() return not vim.g.vscode end },
+	name = 'hlslens' },
 { 'nvim-neorg/neorg',
         run = ':Neorg sync-parsers',
 	ft = 'neorg',
@@ -148,7 +181,9 @@ require('lazy').setup({
 { 'rust-lang/rust.vim',
 	ft = 'rs' },
 },{
-defaults = {}})
+defaults = {
+	cond = fullspec,
+}})
 
 --require('mason').setup()
 --[[
